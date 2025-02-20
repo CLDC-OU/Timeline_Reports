3a22c3ba1e747d7b22a3f63fee3796bec0cb083d
brandonowens24 <brandongerardowens@gmail.com>
2025-02-20 13:40:30 -0500

    update output dir

--- main.py
+++ main.py
@@ -11,9 +11,9 @@ from colorama import Style
 colorama_init()
 
 logfile = f"logs/{dt.now().strftime('%Y-%m-%d_%H-%M-%S')}.log"
-if not os.path.exists("logs"):
+if not os.path.exists("/logs"):
     os.makedirs("logs")
-if not os.path.exists("outcomes"):
+if not os.path.exists("outputs"):
     os.makedirs("outputs")
 logging.basicConfig(
     filename=logfile,
